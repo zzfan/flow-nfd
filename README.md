969956b07fd30341924dd56da564c094a9ff58f6
zzfan <zzfan@mail.ustc.edu.cn>
2016-03-10 09:29:19 +0800

    new flow nfd

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # flow-nfd
-flow based forwarder (based on NFD)
+flow based forwarder (based on [NFD](http://named-data.net/doc/NFD/current)).
+主要的工作是开发基于流的NDN网络架构，改动还是比较多的，主要是针对Interest和PIT.
+由于当时是一个人开发的，文档整理的比较乱，就不放上来了。有时间我会放到我的[Blog](http://blog.zzfanhero.info)上去的。
+如果有什么疑问也可以联系我，在我的Blog上有联系方式，我基本上现在开始每天都会更新Blog了。
+
---
+++ build/config.hpp
@@ -0,0 +1,24 @@
+/* WARNING! All changes made to this file will be lost! */
+
+#ifndef W_CONFIG_HPP_WAF
+#define W_CONFIG_HPP_WAF
+
+#define HAVE_IS_DEFAULT_CONSTRUCTIBLE 1
+#define HAVE_IS_MOVE_CONSTRUCTIBLE 1
+#define HAVE_CXX_OVERRIDE 1
+#define HAVE_CXX_FINAL 1
+#define HAVE_CXX_CLASS_FINAL 1
+#define HAVE_LIBRT 1
+#define HAVE_LIBRESOLV 1
+#define HAVE_PRIVILEGE_DROP_AND_ELEVATE 1
+#define HAVE_IFADDRS_H 1
+#define HAVE_UNIX_SOCKETS 1
+#define HAVE_WEBSOCKET 1
+#define _WEBSOCKETPP_CPP11_STL_ 1
+#define HAVE_ASIO_PCAP_SUPPORT 1
+#define HAVE_LIBPCAP 1
+#define HAVE_PCAP_SET_IMMEDIATE_MODE 1
+#define DEFAULT_CONFIG_FILE "/usr/local/etc/ndn/nfd.conf"
+#define NDEBUG 1
+
+#endif /* W_CONFIG_HPP_WAF */
